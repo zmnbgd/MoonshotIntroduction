7b462d0b6d99f137681d2800dcf603a4f3129bf3
Marko Živanović <32281586+zmnbgd@users.noreply.github.com>
2022-06-23 23:28:38 +0200

    Add files via upload

---
+++ MoonshotIntroductionApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotIntroductionApp.swift
+//  MoonshotIntroduction
+//
+//  Created by Marko Zivanovic on 23.6.22..
+//
+
+import SwiftUI
+
+@main
+struct MoonshotIntroductionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
